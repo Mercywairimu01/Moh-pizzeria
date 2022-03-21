f47543c5488c23aaeca4178a9a641beda85f61aa
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-03-21 09:57:34 +0300

    add functionality to checkout button

--- js/index.js
+++ js/index.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
             $(".display").slideDown();
             $("#home").slideDown();
             $("#grand").slideDown();
+            
             // $("#neworder").hide();
         }
 
@@ -58,8 +59,9 @@ $(document).ready(function(){
         var newCustomer = new Pizza(getSize(), getCrust(), getTopping(), getNumber());
         
         // Total cost for the new customer:
-        var totalCost = ((newCustomer.mySize + newCustomer.myCrust + newCustomer.myTop)*(newCustomer.myQuantity)-100);
+        var totalCost = ((newCustomer.mySize + newCustomer.myCrust + newCustomer.myTop)*(newCustomer.myQuantity));
         $(".display").append("<h3> You have ordered " + newCustomer.myQuantity +" pizza(s) and your Total Bill is: " + totalCost +"</h3>")
+        
     });
 
     //delivery location
@@ -73,11 +75,39 @@ $(document).ready(function(){
 
     // checkout button details
     $("#grand").click(function(){
-        $("#home").hide();
-        $(".display").show();
-        $(".checkout").hide();
+        $("#home").show();
+        $("#grand").hide();
+        $(".display").hide();
+        $(".checkout").show();
         $(".userdeliver").hide();
         $("#neworder").show();
+        $(".table").show()
+        function getSize(){
+            var sizeCost = document.getElementById("pizza").value;
+            return parseInt(sizeCost) 
+        }
+        function getCrust(){
+            var crustCost = document.getElementById("crust").value;
+            return parseInt(crustCost) 
+        }
+        function getNumber(){
+            var myNumber = document.getElementById("quantity").value;
+            return parseInt(myNumber) 
+        }
+        
+        function getTopping() {
+            var myTopping = document.getElementById("topping").value;
+            return parseInt(myTopping);
+          }
+          
+          
+    //    Object for new customer;
+
+        var newCustomer = new Pizza(getSize(), getCrust(), getTopping(), getNumber());
+        
+        // Total cost for the new customer:
+        var totalCost = ((newCustomer.mySize + newCustomer.myCrust + newCustomer.myTop)*(newCustomer.myQuantity));
+       $(".table").append('<tr id="pizzeria"><td id="quantity">'+ newCustomer.myQuantity + '</td><td id="size">' + $(".size option:selected").text()  + '</td><td id="crust">' + $(".crust option:selected").text()  + '</td><td id="topping">' + $(".toppings option:selected").text()+ '</td><td id="total">' + totalCost + '</td></tr>')
     });
     //new order
     $("#neworder").click(function(){
@@ -122,8 +152,7 @@ $(document).ready(function(){
             var newCustomer = new Pizza(getSize(), getCrust(), getTopping(), getNumber());
             
             // Total cost for the new customer:
-            var totalCost = ((newCustomer.mySize + newCustomer.myCrust + newCustomer.myTop)*(newCustomer.myQuantity)-100);
-            
+            var totalCost = ((newCustomer.mySize + newCustomer.myCrust + newCustomer.myTop)*(newCustomer.myQuantity));
             var grandTotal = totalCost+150;
             let customer = $("input#name").val();
             let phone = $("input#phone").val();
@@ -131,7 +160,7 @@ $(document).ready(function(){
     
             if ($("input#name").val() && $("input#phone").val() && $("input#place").val()!=""){
       
-                $(".grandorder").append('<h3>'+ customer+ "You have ordered " + newCustomer.myQuantity +" pizza(s), for " + totalCost + " We have recieved your order and it will be delivered to you at "+area+ ". Prepare Ksh. "+grandTotal +'<h3>');
+                $(".grandorder").append('<h3>'+ customer+ "You have ordered " + newCustomer.myQuantity +" pizza(s), for " + totalCost + " We have recieved your order and it will be delivered to you at "+area+ ". Prepare Ksh. "+ grandTotal +'<h3>');
                  $(".grandorder").slideDown(1000);
                  $(".userdeliver").hide();
                  $("#neworder").show();
